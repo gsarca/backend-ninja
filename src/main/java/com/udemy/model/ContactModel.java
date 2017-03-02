b2d43b35e0d1811ab86dbd597966256a5301a43e
miguelashdez <miguelashdez@gmail.com>
2017-03-01 23:15:52 -0600

    Avance vista, controlador y modelo
    Se sube el avance del controlador ContactController, el modelo
    ContactModel y las vistas contactform y contacts.

--- src/main/java/com/udemy/model/ContactModel.java
+++ src/main/java/com/udemy/model/ContactModel.java
@@ -60,4 +60,10 @@ public class ContactModel {
 	public ContactModel() {
 	}
 
+	@Override
+	public String toString() {
+		return "ContactModel [id=" + id + ", firstname=" + firstname + ", lastname=" + lastname + ", telephone="
+				+ telephone + ", city=" + city + "]";
+	}
+
 }
